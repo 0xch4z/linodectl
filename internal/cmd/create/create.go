52e36a470681aed8edefde197313f9483631926f
Charlie Kenney <Charlesc.kenney@gmail.com>
2021-11-27 18:18:16 -0500

    update create command example

--- internal/cmd/create/create.go
+++ internal/cmd/create/create.go
@@ -8,12 +8,14 @@ import (
 )
 
 const (
-	createExample = `
-	# Create a linode instance
-	linodectl create linode my-linode --region us-east --image ubuntu --type g6-standard-1
+	createExample = `  # Create a linode instance
+  linodectl create linode my-linode --region us-east --image ubuntu --type g6-standard-1
 
-	# Create a linode based on a preset
-	linodectl create linode my-linode --preset prod-machine`
+  # Create a linode based on a preset
+  linodectl create linode my-linode --preset prod-machine
+
+  # Create an LKE Cluster
+  linodectl create lkecluster lke123 -v1.21 --region ap-west --pool g6-standard-3:3`
 )
 
 func NameFromCommandArgs(args []string) (string, error) {
@@ -26,7 +28,7 @@ func NameFromCommandArgs(args []string) (string, error) {
 func NewCmdCreate(f cmdutil.Factory, ioStreams cmdutil.IOStreams) *cobra.Command {
 	cmds := &cobra.Command{
 		Use:     "create [resource]",
-		Short:   "Create a resource",
+		Short:   "Create a Linode resource",
 		Example: createExample,
 		Run: func(cmd *cobra.Command, args []string) {
 			_ = cmd.Help()
